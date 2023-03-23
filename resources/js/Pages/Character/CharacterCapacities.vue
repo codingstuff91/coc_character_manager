39ee23e8311ef2d0efbf102d21a45ad810ce87a1
Matthieu MARTIN <codingstuff91@gmail.com>
2023-03-23 11:00:48 +0100

    Add character way name in character capacities details

--- resources/js/Pages/Character/CharacterCapacities.vue
+++ resources/js/Pages/Character/CharacterCapacities.vue
@@ -10,7 +10,7 @@ defineProps({
         <div class="card">
             <h1 class="card-header">Capacités</h1>
             <div class="capacity mt-4" v-for="capacity in capacities" :key="capacity.id">
-                <h2 class="font-bold text-center text-xl">{{ capacity[0].name }}</h2>
+                <h2 class="font-bold text-center text-xl">{{ capacity[0].character_way.name }}</h2>
                 <div v-for="level in capacity" :key="level.name" class="mt-4">
                     <p class="font-bold mb-2">Niv {{ level.level }}</p>
                     <p class="text-justify">Description {{ level.description }}</p>

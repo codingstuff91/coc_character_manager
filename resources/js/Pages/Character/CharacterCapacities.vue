76d1d57e87c649db51b9c243755fd8eb8cf7533a
Matthieu MARTIN <codingstuff91@gmail.com>
2023-03-23 20:25:21 +0100

    Edit capacity information on show character page

--- resources/js/Pages/Character/CharacterCapacities.vue
+++ resources/js/Pages/Character/CharacterCapacities.vue
@@ -12,8 +12,8 @@ defineProps({
             <div class="capacity mt-4" v-for="capacity in capacities" :key="capacity.id">
                 <h2 class="font-bold text-center text-xl">{{ capacity[0].character_way.name }}</h2>
                 <div v-for="level in capacity" :key="level.name" class="mt-4">
-                    <p class="font-bold mb-2">Niv {{ level.level }}</p>
-                    <p class="text-justify">Description {{ level.description }}</p>
+                    <p class="font-bold mb-2">Niv {{ level.level }} - {{ level.name}}</p>
+                    <p class="text-justify">{{ level.description }}</p>
                 </div>
 
             </div>

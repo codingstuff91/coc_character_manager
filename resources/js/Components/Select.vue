ac08578fda1de846a250fd54ed8f1f7fcb19c5a9
Matthieu MARTIN <codingstuff91@gmail.com>
2023-03-23 10:55:02 +0100

    Add Select vue component

--- resources/js/Components/Select.vue
+++ resources/js/Components/Select.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 defineProps(['modelValue', 'values']);
 
 defineEmits(['update:modelValue']);
@@ -7,11 +7,11 @@ defineEmits(['update:modelValue']);
 <template>
     <select
         class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-lg shadow-sm"
+        @input="$emit('update:modelValue', $event.target.value)"
+        :value="modelValue"
     >
         <option
             v-for="(value, index) in values" :key="index"
-            :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
         >
             {{ value }}
         </option>

95b47332405d19f1a06d5b1e8a254929415fa1b9
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-12-21 12:18:21 +0100

    Add relationship return type on Character model

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Character extends Model
 {
@@ -39,39 +41,39 @@ class Character extends Model
         'family_id'    => 'integer',
     ];
 
-    public function profile()
+    public function profile(): BelongsTo
     {
         return $this->belongsTo(Profile::class);
     }
 
-    public function advantage()
+    public function advantage(): BelongsTo
     {
         return $this->belongsTo(Advantage::class);
     }
 
-    public function family()
+    public function family(): BelongsTo
     {
         return $this->belongsTo(Family::class);
     }
 
-    public function attributes()
+    public function attributes(): BelongsToMany
     {
         return $this->belongsToMany(Attribute::class)
                     ->withPivot('value', 'modificator')
                     ->using(AttributeCharacter::class);
     }
 
-    public function users()
+    public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
     }
 
-    public function weapons()
+    public function weapons(): BelongsToMany
     {
         return $this->belongsToMany(Weapon::class);
     }
 
-    public function capacities()
+    public function capacities(): BelongsToMany
     {
         return $this->belongsToMany(Capacity::class);
     }

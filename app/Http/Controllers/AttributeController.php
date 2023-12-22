d3a02b03e2d135c582d943b09d3cee4e409b11e9
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-12-22 17:49:01 +0100

    Delete useless doc blocks

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -5,14 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Attribute;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class AttributeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $attributes = Attribute::query()
@@ -24,22 +20,11 @@ class AttributeController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return Inertia::render('Admin/Attribute/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         Attribute::create([
@@ -50,12 +35,6 @@ class AttributeController extends Controller
         return to_route('attributes.index');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Attribute $attribute)
     {
         return Inertia::render('Admin/Attribute/Edit', [
@@ -63,13 +42,6 @@ class AttributeController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Attribute $attribute)
     {
         $attribute->update([
@@ -80,12 +52,6 @@ class AttributeController extends Controller
         return to_route('attributes.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  App\Models\Attribute  $attribute
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Attribute $attribute)
     {
         $attribute->delete();

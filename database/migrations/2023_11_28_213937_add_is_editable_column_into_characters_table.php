6f04704853eeaf16a4eed7cf97f236b0f7dfefea
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-12-23 18:45:20 +0100

    Fix migrations styles

--- database/migrations/2023_11_28_213937_add_is_editable_column_into_characters_table.php
+++ database/migrations/2023_11_28_213937_add_is_editable_column_into_characters_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,7 +12,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('characters', function (Blueprint $table) {
+        Schema::table('characters', function(Blueprint $table) {
             $table->boolean('is_editable')->after('avatar')->default(true);
         });
     }
@@ -25,7 +24,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('characters', function (Blueprint $table) {
+        Schema::table('characters', function(Blueprint $table) {
             //
         });
     }

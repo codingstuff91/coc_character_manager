c612e53dbf0445313827a594c396bdd454b2a9fb
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-04-29 09:22:19 +0200

    Add Chronicle model and migrations

---
+++ database/migrations/2025_04_29_071725_add_chronicle_id_column_into_characters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->foreign('chronicle_id')->references('id')->on('chronicles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            //
+        });
+    }
+};

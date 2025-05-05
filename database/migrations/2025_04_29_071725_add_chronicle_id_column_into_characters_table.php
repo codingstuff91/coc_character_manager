23993f4c0e938a3c0c2b141a8a5d100808c372ad
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-05-05 22:24:12 +0200

    Update migration

--- database/migrations/2025_04_29_071725_add_chronicle_id_column_into_characters_table.php
+++ database/migrations/2025_04_29_071725_add_chronicle_id_column_into_characters_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('characters', function(Blueprint $table) {
-            $table->foreignId('chronicle_id')->after('height')->constrained();
+            $table->foreignId('chronicle_id')->constrained();
         });
     }
 };

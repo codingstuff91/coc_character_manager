f90251d69e4b27d86951d3644652f803d1ba6130
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-05-05 22:32:26 +0200

    Change 'chronicle_id' column to unsignedBigInteger in characters table migration

--- database/migrations/2025_04_29_071725_add_chronicle_id_column_into_characters_table.php
+++ database/migrations/2025_04_29_071725_add_chronicle_id_column_into_characters_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('characters', function(Blueprint $table) {
-            $table->foreignId('chronicle_id')->constrained();
+            $table->unsignedBigInteger('chronicle_id')->after('height');
         });
     }
 };

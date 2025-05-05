8752a5e12e29a639ebbdd6243600acd498829235
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-05-05 13:11:52 +0200

    Add link between Character and Chronicle

--- app/Models/Chronicle.php
+++ app/Models/Chronicle.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Chronicle extends Model
 {
@@ -15,5 +16,10 @@ class Chronicle extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'game_master_id');
+    }
+
+    public function characters(): HasMany
+    {
+        return $this->hasMany(Character::class);
     }
 }

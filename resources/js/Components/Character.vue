9bc9dc791d204596a55acae5c4aec951da8712f5
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-05-06 13:42:57 +0200

    Update character card component

--- resources/js/Components/Character.vue
+++ resources/js/Components/Character.vue
@@ -4,14 +4,25 @@ defineProps({
 })
 </script>
 <template>
-    <div class="rounded-lg p-4 bg-slate-400 w-[90%] mx-auto drop-shadow-2xl">
+    <div class="rounded-lg p-4 bg-slate-400 w-[60%] mx-auto drop-shadow-2xl">
         <div class="flex justify-center items-center w-[80%] mx-auto">
-            <img v-if="character.gender == 'M'" src="/img/directeur.png"  class="mr-4" width="55">
-            <img v-else src="/img/femme.png" class="mr-4" width="55">
-            <h1 class="text-white text-xl font-bold">{{ character.name }}</h1>
+            <img
+                v-if="character.gender === 'M'"
+                src="/img/directeur.png"
+                class="mr-4 w-24"
+                alt="male_emoji"
+            >
+            <img
+                v-else
+                src="/img/femme.png"
+                class="mr-4 w-24"
+                alt="male_emoji"
+            >
+
+            <h1 class="text-white text-2xl font-extrabold">{{ character.name }}</h1>
         </div>
         <div class="flex justify-center">
-            <button class="mt-4 p-2 rounded-lg w-[70%] bg-white font-bold text-lg drop-shadow-lg">
+            <button class="mt-4 p-2 rounded-lg w-full bg-white font-bold text-lg drop-shadow-lg">
                 <a :href="route('characters.show', character.id)">Consulter</a>
             </button>
         </div>

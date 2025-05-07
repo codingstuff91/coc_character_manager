9c2b1cf7ee9a53eacbed4df0193156bf92f1e44c
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-05-07 18:41:52 +0200

    Add relationship for character background

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Character extends Model
 {
@@ -70,5 +71,10 @@ class Character extends Model
     public function chronicle(): BelongsTo
     {
         return $this->belongsTo(Chronicle::class);
+    }
+
+    public function background(): HasOne
+    {
+        return $this->hasOne(Background::class);
     }
 }

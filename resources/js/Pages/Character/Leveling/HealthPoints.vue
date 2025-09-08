2931e0a839087ae43f9a0b8ac5e469705f87630d
Matthieu Martin <matthieu.martin@global-exam.com>
2025-09-08 21:21:02 +0200

    New page for health points

---
+++ resources/js/Pages/Character/Leveling/HealthPoints.vue
@@ -0,0 +1,76 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { router } from '@inertiajs/vue3';
+
+import { reactive, onMounted } from "vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import InfoMessage from "@/Components/InfoMessage.vue";
+
+const props = defineProps({
+    healthPoints: Number,
+});
+
+const form = reactive({
+    name: null,
+});
+
+onMounted(() => {
+    form.name = props.healthPoints;
+});
+
+function submitForm () {
+    console.log('test', form)
+    // router.post('/admin/character_ways', form);
+}
+</script>
+
+
+<template>
+    <Head title="Amélioration des PV" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Amélioration PV du personnage</h2>
+        </template>
+
+        <div class="p-6 mx-auto">
+            <form
+                class="w-full flex flex-col"
+                @submit.prevent="submitForm"
+            >
+                <InputLabel class="mt-4" value="Nombre PV supplémentaires"/>
+
+                <TextInput
+                    v-model="form.name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    disabled
+                    required
+                />
+
+                <info-message>
+                    Si Niveau est pair, c'est le résultat du Dé de Vie qui compte.
+                </info-message>
+
+                <info-message>
+                    En cas de modif de Constitution négatif le modif est retranché au résultat de Dé de Vie...
+                </info-message>
+
+                <info-message>
+                    Si Niveau est impair, on prend le modificateur de Constitution.
+                </info-message>
+
+                <button
+                    type="submit"
+                    class="mt-4 px-4 py-2 rounded-lg bg-green-300"
+                >
+                    Confirmer
+                </button>
+            </form>
+        </div>
+
+    </AuthenticatedLayout>
+</template>
+
